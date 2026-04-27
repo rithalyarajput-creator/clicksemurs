75a6a34bf5543d76cbe778cc133a8f2b6b417264
Clicksemurs <hello@clicksemurs.com>
2026-04-27 12:02:03 +0530

    Force cache bust - new build hashes

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost/clicksemurs/api',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]',
+      }
+    }
+  }
 })
